e45dcc00a236c4fcf165e0b532d1a80b0e286b96
Will Schipp <3287360+willschipp@users.noreply.github.com>
2024-09-22 06:51:55 -0500

    WIP: html presentation

--- server/server.py
+++ server/server.py
@@ -1,12 +1,16 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/static/<path:filename>')
 def serve_static(filename):
     root_dir = os.path.dirname(os.getcwd())
-    return send_from_directory(os.path.join(root_dir,'static'),filename)
+    return send_from_directory(os.path.join(root_dir,'static'),"index.html")    
 
 
 
