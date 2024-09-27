f6a9ac07e733c60afe8b79fb22528bc29a974805
Will Schipp <3287360+willschipp@users.noreply.github.com>
2024-09-27 07:33:50 -0500

    WIP: testing for process

--- test/process_test.py
+++ test/process_test.py
@@ -1,16 +1,25 @@
 import unittest
 
+import os
 import threading
 import time
+import uuid
 
 from server.model.form import Form
 from server.model.job import Job
-from server.service.process import createJob, processJob, startBackground, isSelfContained
+from server.service.process import createJob, processJob, startBackground, isSelfContained, noAction
+from server.service.s3 import createBucket, listFiles, cleanUp
 
 class TestProcess(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.secret = os.getenv("SECRET")
+        self.url = "localhost:9000"
+        self.key = "gjUHI2lScQ6JhwnbBkas"
+
+    def tearDown(self):
+        cleanUp(self.url,self.key,self.secret)        
+
 
     # def test_createJob(self):
     #     # create a form
@@ -52,22 +61,51 @@ class TestProcess(unittest.TestCase):
     #     self.assertTrue(background_threads[0].is_alive())
     #     pass
 
-    def test_isSelfContained(self):
-        # create the form
+    # def test_isSelfContained(self):
+    #     # create the form
+    #     form = Form(
+    #         userid="jdoe",
+    #         appid="1234",
+    #         applanguage="java",
+    #         configtext="blah blah blah"
+    #     )
+    #     # create the job object
+    #     job = Job(
+    #         orderid="1234",
+    #         currentStep=0,
+    #         form=form
+    #     )
+    #     # execute
+    #     isSelfContained(job)
+
+    def test_noAction(self):
+        # create a bucket called 1234
+        bucketname = str(uuid.uuid4()).replace('-','')
+        createBucket(bucketname,self.url,self.key,self.secret)
+        # create a form
         form = Form(
             userid="jdoe",
             appid="1234",
             applanguage="java",
             configtext="blah blah blah"
-        )
-        # create the job object
+        )        
+        # create job
         job = Job(
-            orderid="1234",
+            orderid=bucketname,
             currentStep=0,
             form=form
         )
-        # execute
-        isSelfContained(job)
+        # invoke
+        noAction(job)
+        # now check if there's a "finished.json" in the bucket
+        has_file = False
+        files = listFiles(bucketname,self.url,self.key,self.secret)
+        for file in files:
+            if file == "finished.json":
+                has_file = True
+                break
+        self.assertTrue(has_file)
+
 
 
 if __name__ == '__main__':
