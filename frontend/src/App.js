ade13bcd1eeafebcf9dc5da18d51b4aa0549e507
Will Schipp <3287360+willschipp@users.noreply.github.com>
2024-09-28 15:20:48 -0500

    WIP: initial UI framing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+
+// import { Label, TextField, , } from 'react-aria-components';
+import { Grid, View , defaultTheme, Provider, Content, Header, Footer, Heading, Form, TextArea, Button  } from '@adobe/react-spectrum';
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = React.useState({});
 
-  useEffect(() => {
-    fetch('/api/languages')
-      .then(response => response.json())
-      .then(data => setData(data));
-  }, []);
+  // React.useEffect(() => {
+  //   fetch('/api/languages')
+  //     .then(response => response.json())
+  //     .then(data => setData(data));
+  // }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("invoked")
+  }
 
   return (
-    <div className="App">
-      <h1>React + Flask App</h1>
-      <p>Message from Flask: {data.languages}</p>
-    </div>
+    <Provider theme={defaultTheme}>
+      <Grid 
+        areas={['header header', 'sidebar content','footer footer']} 
+        columns={['1fr','3fr']} 
+        rows={['size-1000','auto','size-1000']} 
+        height="100vh" 
+        gap="size-100">
+        <View gridArea="header">
+          <Header>
+            <Heading level={2}>Container Readiness Assessment Platform</Heading>
+          </Header>
+        </View>
+        <View gridArea="sidebar">
+          <Button>Check on existing Order</Button>
+        </View>
+        <View gridArea="content">   
+        <Content>      
+          <Form onSubmit={handleSubmit} maxWidth="size-5000">
+            <TextArea label="Copy & Paste your build file here"/>
+            <Button variant="accent" type="submit" maxWidth="size-1000">Submit</Button>
+          </Form>
+          </Content>
+        </View>
+        <View gridArea="footer">
+          <Footer>v0.0.1</Footer>
+        </View>
+      </Grid>
+    </Provider>
   );
 }
 
